6be3dea7903f2dc7ff5a670c967d6f185891e9ad
Oli Evans <oli@tableflip.io>
2018-04-30 10:08:19 +0100

    Init a WebDB

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log('hello dat')
+const WebDB = require('@beaker/webdb')
+
+async function initDb () {
+  let url = localStorage.getItem('my-dat')
+  if (!url) {
+    const archive = await DatArchive.create({
+      title:'make-dat-lunch'
+    })
+    url = archive.url
+    localStorage.setItem('my-dat', url)
+  }
+  const webdb = new WebDB('make-dat-lunch')
+  await webdb.indexArchive(url)
+  return webdb
+}
+
+initDb().then(webdb => {
+  window.webdb = webdb
+})
